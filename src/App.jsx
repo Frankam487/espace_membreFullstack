78c0033adbc8352ee5b7e99d5832e24d32f43af7
Frankam487 <fkamgang06@gmail.com>
2025-05-10 01:12:05 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import { useState } from "react"
-
-export function App() {
-  const [data, setData] = useState();
+import React from 'react';
+export default function App() {
+  const [data, setData] = useState(0);
   const handleClickUp = () => {
-    console.log('ok');
-    
+    setData((t) => t+1);
+    data === 10 && alert('tu es trop loin bro!!!');
+    return null;
   }
   const handleClickDown = () => {
-    console.log('ok');
+    setData((t) => t-1);
+    data === -4 && alert('tu es trop bas !!!')
+    return null;
     
   }
   return (
-    <div className="bg-cyan-200">
-      <button onClick={handleClickUp}>Clic moi pour monter</button>
-      <h1></h1>
-      <button onClick={handleClickDown}>Clic moi pour descendre</button>
+    <div className="w-1/2 my-10 mx-auto bg-red-900 flex flex-col rounded">
+      <button className="p-2 bg-rose-500 btn-accent hover:p-[12px] transition-all" onClick={handleClickUp}>Clic moi pour monter</button>
+      <h1 className="text-center">{ data}</h1>
+      <button onClick={handleClickDown} className="p-2 bg-rose-500 btn-accent hover:p-[12px] transition-all">Clic moi pour descendre</button>
     </div>
   )
 }
